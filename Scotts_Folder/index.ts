3cedfd587aa964eb49ce71f4d3a6a4ef5d6b1365
miguelmendez <miguelmendez1208@gmail.com>
2024-10-05 21:24:00 -0500

    fixed index.ts to serve files correctly.

--- Scotts_Folder/index.ts
+++ Scotts_Folder/index.ts
@@ -3,13 +3,22 @@
 const serve =Bun.serve({
     port:5080,
     async fetch(req) {
-      return new Response(await Bun.file("./index.html").bytes(), {
-        headers: {
-          "Content-Type": "text/html",
-        },
-      })
-    },
+        const url = new URL(req.url);
+        if (url.pathname === "/game.js"){
+            const gamejs = "./game.js";
+            const gamefile = Bun.file(gamejs);
+            console.log("game?");
+            return new Response(gamefile); 
+        } 
+        const filepath = url;
+        const loaded_file = Bun.file(filepath);
+        return new Response(loaded_file);
+
+        console.log("new request?");
+        const path = "./index.html";
+        const file = Bun.file(path);
+        return new Response(file);
+    }
 });
 
-  console.log("Hello listening at? : ");
-  console.log(serve.port);
+  console.log("Hello listening at? : " + serve.port);
